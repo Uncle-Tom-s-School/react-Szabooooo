c1866a4a6249f3abbcfe3fafe2e6c8a070059c87
Oláh Szabolcs <szabi@example.com>
2025-10-21 09:00:40 +0200

    kesz

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from "react"
 import BeerCard from "./components/BeerCard"
 
+export type BeerCardType ={
+    image:string,
+    name: string,
+    abv: number,
+    volume: number,
+    price: number,
+    available: boolean
+}
+
+
+
+
 const App = () => {
+
+  const [data,setData] = useState<BeerCardType[]>([])
+  useEffect(()=>{
+    fetch("data.json")
+    .then(res => res.json())
+    .then(data => setData(data))
+  },[])
   return (
     <div className="beerCardWrapper">
-      <BeerCard />
+      {data.map((item) => <BeerCard{...item}/>)}
     </div>
   )
 }
--- src/components/BeerCard.tsx
+++ src/components/BeerCard.tsx
@@ -1,6 +1,14 @@
-const BeerCard = () => {
+import type { BeerCardType } from "../App"
+
+const BeerCard = (props: BeerCardType) => {
   return (
-    <div className='beerCard'>Beer Card</div>
+    
+    <div className='beerCard'>
+      <img src={props.image} alt="" />
+      <h3>{props.name} {props.abv}% {props.volume}l</h3>
+      <p>{props.price}</p>
+      <b className={props.available ? "available" : "notAvailable"}>{props.available ? "Raktáron" : "Nincs raktáron"}</b>
+    </div>
   )
 }
 
