8cfb76db4c439ce288bb5e294bca3d9b9aff4a75
Oláh Szabolcs <szabi@example.com>
2025-10-21 09:01:57 +0200

    keszharom

--- src/components/BeerCard.tsx
+++ src/components/BeerCard.tsx
@@ -5,7 +5,7 @@ const BeerCard = (props: BeerCardType) => {
     
     <div className='beerCard'>
       <img src={props.image} alt="" />
-      <h3>{props.name} {props.abv} {props.volume}l</h3>
+      <h3>{props.name} {props.abv} {props.volume}</h3>
       <p>{props.price}</p>
       <b className={props.available ? "available" : "notAvailable"}>{props.available ? "Raktáron" : "Nincs raktáron"}</b>
     </div>
